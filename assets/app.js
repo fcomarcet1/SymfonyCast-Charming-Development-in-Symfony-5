edc433e2fc7e90d992df5645b4348725f96284d3
fcomarcet1 <fcomarcet1@gmail.com>
2022-09-22 10:00:28 +0200

    Moving js scripts Code into Encore(app.js)

--- assets/app.js
+++ assets/app.js
@@ -4,12 +4,29 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
 
+/**
+ * Simple (ugly) code to handle the comment vote up/down
+ */
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// select element .js-vote-arrows
+var $container = $('.js-vote-arrows');
+// find <a> elements inside the container on click event
+$container.find('a').on('click', function(e) {
+    e.preventDefault();
+    var $link = $(e.currentTarget);
+    $.ajax({
+        url: '/comments/10/vote/'+$link.data('direction'),
+        method: 'POST'
+    }).then(function(data) {
+        console.log(data);
+        // use field votes because in controller we return votes
+        $container.find('.js-vote-total').text(data.votes);
+    });
+});
